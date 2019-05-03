a8758831928cf5f6e705d57c72e7d47318a7379d
taufikanhar <taufikanhar@gmail.com>
2019-05-03 16:49:14 +0700

    first commit

---
+++ src/day01/StudyCase01Soal07.java
@@ -0,0 +1,28 @@
+package day01;
+
+import java.util.Scanner;
+
+public class StudyCase01Soal07 {
+
+	protected static Scanner input;
+	
+	public static void main(String[] args) {
+		
+		input = new Scanner(System.in);
+		
+		System.out.println("Masukan Nomer: ");
+		
+		int n = Integer.parseInt(input.nextLine());
+		
+		int number=2;
+		
+		for (int i=0; i <n; i++) {
+			
+			System.out.print(number + "\t");
+			
+			number=number*2;
+			
+		}
+	}
+
+}
