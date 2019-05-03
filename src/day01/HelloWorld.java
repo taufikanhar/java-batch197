bd1b2072bb366d3d1c2f36c64c105c66a1a7661f
taufikanhar <taufikanhar@gmail.com>
2019-05-03 10:38:37 +0700

    first commit

---
+++ src/day01/HelloWorld.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, welcome to bootcamp class");
+		// TODO Auto-generated method stub
+
+	}
+
+}
